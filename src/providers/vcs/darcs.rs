c8f7291894c23519a322b2cd1887a619e016f34c
Nicola Bonelli <nicola.bonelli@gmail.com>
2025-09-27 18:42:03 +0200

    Implement Darcs VCS provider with status and commit info

--- src/providers/vcs/darcs.rs
+++ src/providers/vcs/darcs.rs
@@ -1,33 +1,69 @@
-use std::path::Path;
-
+use crate::{cmd::CMD, providers::vcs::{merge_icons, StatusIcon}};
+use std::{path::Path, str::FromStr};
 use crate::{chunk::Chunk, options::Options, providers::vcs::VcsTrait};
-use smol_str::SmolStr;
+use smol_str::{SmolStr, ToSmolStr};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Darcs;
 
+macro_rules! darcs {
+    ( $( $x:expr ),* ) => {
+        CMD.exec("darcs", [$( $x ),*])
+    };
+}
+
 impl VcsTrait for Darcs {
     async fn branch(&self, _opts: &Options, _path: &Path) -> Option<Chunk<SmolStr>> {
-        todo!()
+        None
     }
 
     async fn commit(&self, _opts: &Options, _path: &Path) -> Option<Chunk<SmolStr>> {
-        todo!()
+        darcs!("log", "--last", "1", "--summary")
+            .await?
+            .lines()
+            .next()?
+            .split_whitespace()
+            .nth(1)
+            .map(|s| Chunk::new("⭑", s.trim().to_smolstr()))
     }
 
     async fn status(&self, _opts: &Options, _path: &Path) -> Option<Chunk<SmolStr>> {
-        todo!()
+        darcs!("whatsnew", "-s")
+            .await
+            .filter(|s| !s.is_empty())
+            .map(|s| {
+                Chunk::info(merge_icons(
+                    s.lines()
+                        .map(|l| l.parse::<StatusIcon<Darcs>>().unwrap())
+                        .collect::<Vec<_>>(),
+                ))
+            })
     }
 
     async fn worktree(&self, _opts: &Options, _path: &Path) -> Option<Chunk<SmolStr>> {
-        todo!()
+        None
     }
 
     async fn stash(&self, _opts: &Options, _path: &Path) -> Option<Chunk<SmolStr>> {
-        todo!()
+        None
     }
 
     async fn divergence(&self, _opts: &Options, _path: &Path) -> Option<Chunk<SmolStr>> {
-        todo!()
+        None
+    }
+}
+
+impl FromStr for StatusIcon<Darcs> {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let chars = s.split_whitespace().next();
+        match chars {
+            Some("R") => Ok(StatusIcon::new("−")),
+            Some("A") => Ok(StatusIcon::new("✚")),
+            Some("M") => Ok(StatusIcon::new("●")),
+            Some("F") => Ok(StatusIcon::new("→")),
+            Some("T") => Ok(StatusIcon::new("→")),
+            _ => Ok(StatusIcon::new("")), // Unknown state
+        }
     }
 }
